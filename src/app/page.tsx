f5d44559c6216af8c36d3aa3f426350052f7fa84
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-21 15:51:20 -0300

    feat: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Modal from "@/components/Modal/Modal";
 import RegisterDebt from "@/components/RegisterDebt/RegisterDebt";
+import Contas from "./contas/page";
 
 export default function Home() {
 
   return (
-    <div className="px-32">
-      <RegisterDebt />
+    <div>
+      <Modal/>
+      <Contas/>
     </div>
   );
 }
