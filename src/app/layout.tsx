e3140b23922fb5dd861d15f55d3d36ad15177d52
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-22 13:45:13 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,32 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
-import { Toaster } from "@/components/ui/toaster"
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
-import Header from "@/components/Header/Header"
+import Header from "@/components/Header/Header";
 
+export const metadata: Metadata = {
+  title: "Controle Comércio",
+  description: "Web app para controlar dívidas de clientes",
+};
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
-export const metadata: Metadata = {
-  title: "Controle Comércio",
-  description: "Web app para controlar divídas de clientes",
-};
+const RootLayout: React.FC = ({ children }) => (
+  <html lang="en">
+    <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <Toaster />
+      <main>
+        <Header />
+        {children}
+      </main>
+    </body>
+  </html>
+);
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased",fontSans.variable)}>
-        <Toaster />
-        <main>
-          <Header />
-          {children}
-        </main>  
-      </body>
-    </html>
-  );
-}
+export default RootLayout;
