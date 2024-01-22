27197c2da328c9a541fb12b5ae99e811c83235b5
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-22 10:56:41 -0300

    hotfix inputPrimario

--- src/components/InputPrimario.tsx
+++ src/components/InputPrimario.tsx
@@ -5,12 +5,13 @@ import { Dispatch, SetStateAction } from "react"
 interface InputPrimarioProps {
     label: string
     onChange: Dispatch<SetStateAction<string>>
+    branco?: boolean
 }
  
-export default function InputPrimario({label,onChange} : InputPrimarioProps) {
+export default function InputPrimario({label,onChange,branco} : InputPrimarioProps) {
   return (
     <div className="w-full flex flex-col gap-2">
-      <Label htmlFor="email">{label}</Label>
+      <Label className={`${branco ? 'text-white' : 'text-black'}`} htmlFor="email">{label}</Label>
       <Input className="text-black" onChange={(e) => onChange(e.target.value)} type="text" id="text" placeholder={`${label}`} />
     </div>
   )
