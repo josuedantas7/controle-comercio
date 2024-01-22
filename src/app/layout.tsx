fa3f4784765b72fb0cfa31d81638cc593d7809ba
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-01-22 13:50:13 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,29 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster"
 
-import Header from "@/components/Header/Header";
+import Header from "@/components/Header/Header"
 
 export const metadata: Metadata = {
   title: "Controle Comércio",
-  description: "Web app para controlar dívidas de clientes",
+  description: "Web app para controlar divídas de clientes",
 };
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-const RootLayout: React.FC = ({ children }) => (
-  <html lang="en">
-    <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-      <Toaster />
-      <main>
-        <Header />
-        {children}
-      </main>
-    </body>
-  </html>
-);
-
-export default RootLayout;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={"min-h-screen bg-background font-sans antialiased"}>
+        <Toaster />
+        <main>
+          <Header />
+          {children}
+        </main>  
+      </body>
+    </html>
+  );
+}
